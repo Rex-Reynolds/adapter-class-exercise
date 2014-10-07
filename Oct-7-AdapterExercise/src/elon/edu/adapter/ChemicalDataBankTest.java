4739124e3e1e2aa2eb818cb12271ff0f206fe6c1
Dave Powell <rreynolds3@elon.edu>
2014-10-07 14:05:45 -0400

    asd

--- Oct-7-AdapterExercise/src/elon/edu/adapter/ChemicalDataBankTest.java
+++ Oct-7-AdapterExercise/src/elon/edu/adapter/ChemicalDataBankTest.java
@@ -14,12 +14,15 @@ import org.junit.Test;
  *
  */
 public class ChemicalDataBankTest {
+	private ChemicalDataBank cBank;
+	private float expected;
 
 	/**
 	 * @throws java.lang.Exception
 	 */
 	@Before
 	public void setUp() throws Exception {
+		cBank = new ChemicalDataBank();
 	}
 
 	/**
@@ -27,18 +30,25 @@ public class ChemicalDataBankTest {
 	 */
 	@After
 	public void tearDown() throws Exception {
+		cBank = null;
+		expected = 0.0f;
 	}
 
 	/**
-	 * Test method for {@link elon.edu.adapter.ChemicalDataBank#getCriticalPoint(java.lang.String, java.lang.String)}.
+	 * Test method for
+	 * {@link elon.edu.adapter.ChemicalDataBank#getCriticalPoint(java.lang.String, java.lang.String)}
+	 * .
 	 */
 	@Test
 	public void testGetCriticalPoint() {
-		fail("Not yet implemented");
+		expected = 100.0f;
+		assertEquals(expected, cBank.getCriticalPoint("Water", "F"),0);
 	}
 
 	/**
-	 * Test method for {@link elon.edu.adapter.ChemicalDataBank#getMolecularStructure(java.lang.String)}.
+	 * Test method for
+	 * {@link elon.edu.adapter.ChemicalDataBank#getMolecularStructure(java.lang.String)}
+	 * .
 	 */
 	@Test
 	public void testGetMolecularStructure() {
@@ -46,7 +56,9 @@ public class ChemicalDataBankTest {
 	}
 
 	/**
-	 * Test method for {@link elon.edu.adapter.ChemicalDataBank#getMolecularWeight(java.lang.String)}.
+	 * Test method for
+	 * {@link elon.edu.adapter.ChemicalDataBank#getMolecularWeight(java.lang.String)}
+	 * .
 	 */
 	@Test
 	public void testGetMolecularWeight() {
